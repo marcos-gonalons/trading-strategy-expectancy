37cd7d34edf13a90d65da87421e6c07398cb6c48
Marcos Gonalons <m.gonalons.camps@gmail.com>
2020-05-20 15:57:43 +0200

    Improved css

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,10 +3,12 @@ import { Container, Grid } from "@material-ui/core";
 import Form from "../../components/form/Form";
 import Result from "../../components/result/Result";
 
+import styles from "./index.module.css";
+
 function Index(): JSX.Element {
 	return (
 		<Container maxWidth="lg">
-			<Grid container spacing={3}>
+			<Grid container spacing={3} id={styles["main-container"]}>
 				<Grid item sm={12} md={6}>
 					<Form />
 				</Grid>
